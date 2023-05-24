56176e1ae22795e3a87f25cc63f6e09257519820
CarolAlvares23 <122477075+CarolAlvares23@users.noreply.github.com>
2023-05-24 17:51:23 -0300

    Update ajuste_contraste.py

--- pillow/ajuste_contraste.py
+++ pillow/ajuste_contraste.py
@@ -1,22 +1,13 @@
 import cv2
 
-# Carrega a imagem
 image = cv2.imread('images/ajuste_contraste8.jpg')
+alpha = 1.5  
+beta = 0    
 
-# Define os parâmetros de ajuste de contraste
-alpha = 1.5  # Fator de multiplicação para aumentar o contraste
-beta = 0    # Valor a ser adicionado para ajustar o brilho
 
-# Realiza o ajuste de contraste
 imagem_ajustada = cv2.convertScaleAbs(image, alpha=alpha, beta=beta)
-
-# Exibe a imagem original e a imagem ajustada
 cv2.imshow('Imagem Original', image)
 cv2.imshow('Imagem com Ajuste de Contraste', imagem_ajustada)
-
-# Aguarda até que uma tecla seja pressionada
 cv2.waitKey(0)
-
-# Fecha todas as janelas abertas
 cv2.destroyAllWindows()
 cv2.imwrite('images/ajuste_contraste8.jpg_imwrite', imagem_ajustada)
